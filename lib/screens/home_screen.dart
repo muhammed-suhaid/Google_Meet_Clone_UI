9c2945cedc3c27ebde6f63d8725ec6c55c8070a4
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-08-07 20:06:47 +0530

    Home screen completed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -74,7 +74,7 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              const HistoryCard()
+              const HistoryCard(),
             ],
           ),
         ),
